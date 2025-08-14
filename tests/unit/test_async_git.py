0261873a1b198ccbda575313c372b4bbf3b422cf
Felipe Zuniga Carlier <felipec@trackonomysystems.com>
2025-08-14 12:00:28 -0700

    Updated unit tests

--- tests/unit/test_async_git.py
+++ tests/unit/test_async_git.py
@@ -1,7 +1,6 @@
+import asyncio
 import pytest
-
-# from unittest.mock import AsyncMock
-
+from unittest.mock import AsyncMock
 from mono_merger.async_git import AsyncGitRepo
 
 
@@ -34,7 +33,6 @@ async def test_commit(mocker, sample_config):
 @pytest.mark.asyncio
 async def test_subtree_add(mocker, sample_config):
     mock_run_git_command = mocker.patch.object(AsyncGitRepo, "_run_git_command")
-
     mock_async_git = AsyncGitRepo(sample_config.output_dir)
 
     repo = sample_config.repos[0]
@@ -42,7 +40,6 @@ async def test_subtree_add(mocker, sample_config):
     prefix = f"{branch.domain}/{branch.name}"
 
     await mock_async_git.subtree_add(prefix, repo.url, branch.name, True)
-
     mock_run_git_command.assert_called_once_with(
         "subtree",
         "add",
@@ -52,4 +49,26 @@ async def test_subtree_add(mocker, sample_config):
         branch.name,
         "--squash",
         timeout=600,
+    )
+
+
+@pytest.mark.asyncio
+async def test_run_git_command(mocker, sample_config):
+    mock_process = AsyncMock()
+    mock_process.communicate.return_value = (b"success", b"")
+    mock_process.returncode = 0
+
+    mock_create_subprocess = mocker.patch("asyncio.create_subprocess_exec")
+    mock_create_subprocess.return_value = mock_process
+
+    mock_async_git = AsyncGitRepo(sample_config.output_dir)
+    result = await mock_async_git._run_git_command("status")
+    assert result == "success"
+
+    mock_create_subprocess.assert_called_once_with(
+        "git",
+        "status",
+        cwd=mock_async_git.repo_path,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
     )
